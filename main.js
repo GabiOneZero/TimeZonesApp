6e8fa45c22a0c4cc3631ae7405f2794753675106
Gabriel Sánchez <gabionezero@gmail.com>
2022-09-06 07:40:56 +0200

    Take form data

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//Function -> Create querySelector for multiple uses
+const $ = selector => document.querySelector(selector)
 
+//Recover form datas
+$('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  //Object (JSON) from Array[arrays]
+  const data = Object.fromEntries(new window.FormData(event.target))
+  console.log(data)
+})
